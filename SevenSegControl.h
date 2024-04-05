1cfa632920a1f11280397c9b1c8d58c2ea8edb63
Jorge Muñoz Rodríguez <91954520+jormunrod@users.noreply.github.com>
2024-04-05 17:20:38 +0200

    Update SevenSegControl.h

--- SevenSegControl.h
+++ SevenSegControl.h
@@ -9,14 +9,15 @@ public:
     ~SevenSegControl();
     void displayNumber(int number);
 private:
-    int* _digitPins;
-    int* _segmentPins;
-    int _numDigits;
-    unsigned long _updateInterval;
-    void clean();
-    void setNumber(uint8_t segments);
-    void displayDigit(int digit, int value);
+    int* _digitPins; // Puntero a un array que almacena los pines de los dígitos
+    int* _segmentPins; // Puntero a un array que almacena los pines de los segmentos
+    int _numDigits; // Número de dígitos en el display
+    unsigned long _updateInterval; // Intervalo de actualización en microsegundos basado en la frecuencia
+    void clean(); // Método para limpiar/apagar todos los segmentos
+    void setNumber(uint8_t segments); // Método para establecer un número en los segmentos
+    void displayDigit(int digit, int value); // Método para mostrar un dígito
 };
+
 
 
 #endif
